124d8509dd80a1b351ff92574a1d3ae2d4887b11
mharun07 <rasyidharunar0@gmail.com>
2021-08-23 12:54:38 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,87 @@
+let numberItem = document.querySelectorAll('.number-item');
+let operatorItem = document.querySelectorAll('.item-red');
+let inputCalculatorKecil = document.querySelector('.result-input-kecil');
+let inputCalculatorBesar = document.querySelector('.result-input-besar');
+let removeAllItem = document.querySelector('.remove-all-item');
+let removeOneItem = document.querySelector('.remove-one-item');
+let resultItem = document.querySelector('.result-item');
+
+let inputNumber = '';
+
+numberItem.forEach(m => {
+    m.addEventListener('click', function () {
+        inputNumber = m.dataset.value
+        if (inputCalculatorKecil.value == '0') inputCalculatorKecil.value = '';
+
+        inputCalculatorKecil.value += inputNumber;
+    })
+});
+
+operatorItem.forEach(m => {
+    let inputOperatorValue = m.dataset.value;
+
+    m.addEventListener('click', function () {
+        const inputOperator = /[+*\/-]/g;
+        let inputKecilOperator = inputCalculatorKecil.value.match(inputOperator);
+
+        if (inputKecilOperator == null) {
+            inputCalculatorKecil.value += inputOperatorValue;
+        } else {
+            if (inputCalculatorKecil.value.slice(-1).match(inputOperator)) {
+                let removeInputOperator = inputCalculatorKecil.value.split('').reverse().join('').substr(1, inputCalculatorKecil.value.length);
+                inputCalculatorKecil.value = removeInputOperator.split('').reverse().join('');
+                inputCalculatorKecil.value += inputOperatorValue;
+                inputKecilOperator = null
+            } else {
+                inputCalculatorKecil.value += inputOperatorValue;
+                inputKecilOperator = null
+            }
+        }
+    })
+});
+
+removeAllItem.addEventListener('click', function () {
+    inputCalculatorKecil.value = '0';
+    inputCalculatorBesar.value = '0';
+})
+
+removeOneItem.addEventListener('click', function () {
+    if (inputCalculatorKecil.value == '0') {
+        inputCalculatorKecil.value = '0'
+    } else {
+        let inputValue = inputCalculatorKecil.value;
+        if (inputValue.length == 1) {
+            inputCalculatorKecil.value = '0';
+        } else {
+            inputValue = inputValue.split('').reverse().join('').substr(1, inputValue.length);
+            inputCalculatorKecil.value = inputValue.split('').reverse().join('');
+        }
+    }
+})
+
+resultItem.addEventListener('click', function () {
+    const inputOutput = inputCalculatorKecil.value;
+    const inputOperator = /[+*\/-]/g;
+
+    if (inputOutput.slice(-1).match(inputOperator)) {
+        let errorText = `ERROR\n${inputOutput}?\nPERSAMAAN TIDAK LENGKAP !`;
+        alert(errorText);
+
+        inputCalculatorKecil.value = '0'
+        inputCalculatorBesar.value = '0';
+    } else {
+        let result = eval(inputOutput);
+        inputCalculatorBesar.value = result;
+    }
+})
+
+let btnLightTheme = document.querySelector('.light-theme');
+let btnDarkTheme = document.querySelector('.dark-theme');
+
+btnLightTheme.addEventListener('click', function () {
+    document.body.setAttribute('data-theme', 'light');
+})
+
+btnDarkTheme.addEventListener('click', function () {
+    document.body.setAttribute('data-theme', '');
+})
